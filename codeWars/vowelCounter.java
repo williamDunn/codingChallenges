20c19570de33f6135695b282943ba468a4bd13e9
Will <37053782+williamDunn@users.noreply.github.com>
2020-12-18 17:08:10 -0600

    Update vowelCounter.java

--- codeWars/vowelCounter.java
+++ codeWars/vowelCounter.java
@@ -1,25 +1,24 @@
-public static void main(String[] args) {
-        	
-    	String str = "abracadabra";
+public static void main(String[] args) 
+{
+	String str = "abracadabra";
     	
-    	int vowelCount = 0;
+	int vowelCount = 0;
     	
-    	char[] letters = new char[str.length()];
+	char[] letters = new char[str.length()];
     	
-    	for(int i = 0; i < str.length(); i++)
-    	{
-    		letters[i] = str.charAt(i);
-    		System.out.println(letters[i]);
+	for(int i = 0; i < str.length(); i++)
+	{
+		letters[i] = str.charAt(i);
+		System.out.println(letters[i]);
     		
-    		if(letters[i] == 'a' || letters[i] == 'e' || letters[i] == 'i' || letters[i] == 'o' || letters[i] == 'u')
-    		{
-    			vowelCount++;
-    		}
-    		else
-    		{
-    			continue;
-    		}
-    	}  	
-    	
-    	System.out.println(vowelCount);
-	}
+		if(letters[i] == 'a' || letters[i] == 'e' || letters[i] == 'i' || letters[i] == 'o' || letters[i] == 'u')
+		{
+			vowelCount++;
+		}
+		else
+		{
+			continue;
+		}
+	}  	
+	System.out.println(vowelCount);
+}
